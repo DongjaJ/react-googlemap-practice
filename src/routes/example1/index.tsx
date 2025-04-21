fc1e860a584ab2d03299cbbbb38e6d6330598aaa
DongjaJ <jdh981118@naver.com>
2025-04-21 23:51:28 +0900

    feat: 예시 1 화면 지도

--- src/routes/example1/index.tsx
+++ src/routes/example1/index.tsx
@@ -1,9 +1,26 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { Suspense } from "react";
+import { ClusteredMarkers } from "../../components/example1/clustered-markers";
 
 export const Route = createFileRoute("/example1/")({
   component: Example1Page,
 });
 
 function Example1Page() {
-  return <div className='p-2'>Hello from About!</div>;
+  return (
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAP_KEY} version={"beta"}>
+      <Map
+        defaultCenter={{ lat: 37.498454033368, lng: 127.03229336072 }}
+        defaultZoom={15}
+        gestureHandling={"greedy"}
+        disableDefaultUI
+        className='w-screen h-screen'
+      >
+        <Suspense>
+          <ClusteredMarkers />
+        </Suspense>
+      </Map>
+    </APIProvider>
+  );
 }
